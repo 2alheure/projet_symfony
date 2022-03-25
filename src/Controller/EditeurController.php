6e10030eaaf00af06fe438d3ee54c40de1b75de2
2alheure <jojo77.juv@live.fr>
2022-03-25 12:57:05 +0100

    Fin CRUD Editeur

--- src/Controller/EditeurController.php
+++ src/Controller/EditeurController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Editeur;
+use App\Form\EditeurType;
 use App\Repository\EditeurRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EditeurController extends AbstractController {
     #[Route('/editeurs', name: 'app_editeurs_liste')]
@@ -15,6 +18,44 @@ class EditeurController extends AbstractController {
         return $this->render('editeur/liste.html.twig', [
             'editeurs' => $editeurs,
         ]);
+    }
+
+    #[Route('/editeurs/create', name: 'app_editeurs_create')]
+    public function create(EditeurRepository $er, Request $request) {
+
+        $editeur = new Editeur;
+
+        $formulaire = $this->createForm(EditeurType::class, $editeur);
+
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $er->add($editeur);
+            return $this->redirectToRoute('app_editeurs_liste');
+        } else {
+            return $this->render('editeur/formulaire.html.twig', [
+                'formView' => $formulaire->createView(),
+            ]);
+        }
+    }
+
+    #[Route('/editeurs/{id}/update', name: 'app_editeurs_update')]
+    public function update($id, EditeurRepository $er, Request $request) {
+
+        $editeur = $er->find($id);
+
+        $formulaire = $this->createForm(EditeurType::class, $editeur);
+
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $er->add($editeur);
+            return $this->redirectToRoute('app_editeurs_liste');
+        } else {
+            return $this->render('editeur/formulaire.html.twig', [
+                'formView' => $formulaire->createView(),
+            ]);
+        }
     }
 
     #[Route('/editeurs/{id}', name: 'app_editeurs_details')]
