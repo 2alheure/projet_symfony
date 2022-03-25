4e6dc7909aa7592f2ed64a5c02725826be2d570a
2alheure <jojo77.juv@live.fr>
2022-03-25 12:07:07 +0100

    CGU

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController {
 
     /**
-     * @Route("/home", name="accueil")
+     * @Route("/", name="accueil")
      */
     public function home(): Response {
         $prenom = 'Jordan';
